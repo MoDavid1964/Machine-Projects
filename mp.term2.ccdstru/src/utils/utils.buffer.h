c74f36d1cdb04772b1370b8efda5e1daf2e3f334
Mo David <modavid.1964@gmail.com>
2024-02-08 23:57:23 +0800

    mp.term2.ccdstru: Updated the buffer class.

--- mp.term2.ccdstru/src/utils/utils.buffer.h
+++ mp.term2.ccdstru/src/utils/utils.buffer.h
@@ -8,7 +8,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define BUFFER_MAX 1024
+// Maximum length of a buffer
+// Includes characters not rendered to the screen, such as:
+//    (1) ANSI Escape Sequences
+//    (2) The null byte
+#define BUFFER_MAX_LENGTH 1024
 
 /**
  * A class that stores a single buffer of text and a pointer to another buffer of text.
@@ -18,17 +22,18 @@
 typedef struct Buffer Buffer;
 
 struct Buffer {
-  char *sText;  // The content of the buffer instance
-  int dWidth;   // The render length of the stored string (excluding ANSI escape sequences)
-  int dHeight;  // The height of the chain of buffers; only has a value if the buffer node represents the head
+  char *sText;        // The content of the buffer instance
+  int dWidth;         // The render length of the stored string (excluding ANSI escape sequences)
+  int dRenderWidth;   // The maximum number of *rendered* characters in the string
+  int dHeight;        // The height of the chain of buffers; only has a value if the buffer node represents the head
 
   Buffer *pNext;  // A reference to an adjacent node
 };
 
 // Constructors and destructors
 Buffer *Buffer_new();
-Buffer *Buffer_init(Buffer *this);
-Buffer *Buffer_create();
+Buffer *Buffer_init(Buffer *this, int dRenderWidth);
+Buffer *Buffer_create(int dRenderWidth);
 void Buffer_kill(Buffer *this);
 
 // Getters and setters
@@ -64,8 +69,11 @@ Buffer *Buffer_new() {
 /**
  * Initializes an instance of the buffer class.
 */
-Buffer *Buffer_init(Buffer *this) {
+Buffer *Buffer_init(Buffer *this, int dRenderWidth) {
+  this->sText = calloc(BUFFER_MAX_LENGTH, sizeof(char));
+
   this->dWidth = 0;
+  this->dRenderWidth = dRenderWidth;
   this->dHeight = 0;
 
   return this;
@@ -76,10 +84,10 @@ Buffer *Buffer_init(Buffer *this) {
  * 
  * @return  { Buffer * }  A pointer to the created instance.
 */
-Buffer *Buffer_create() {
+Buffer *Buffer_create(int dRenderWidth) {
   Buffer *pBuffer = Buffer_new();
 
-  Buffer_init(pBuffer);
+  Buffer_init(pBuffer, dRenderWidth);
 
   return pBuffer;
 }
@@ -101,17 +109,25 @@ void Buffer_kill(Buffer *this) {
  * ////////// 
 */
 
+/**
+ * Returns a reference to the text stored by the buffer.
+ * 
+ * @param   { Buffer * }  this  The buffer instance to be read.
+ * @return  { char * }          The pointer to the string stored by the buffer.
+*/
 char *Buffer_getText(Buffer *this) {
   return this->sText;
 }
 
+/**
+ * Replaces the contents of the string in the buffer.
+ * 
+ * @param   { Buffer * }  this      The buffer instance to be modified.
+ * @param   { char * }    sNewText  The new string to be copied.
+*/
 void Buffer_setText(Buffer *this, char *sNewText) {
-  int dBytes = snprintf(NULL, 0, "%s", sNewText) + 1;
-
   Buffer_clearText(this);
-  this->sText = malloc(dBytes);
-
-  snprintf(this->sText, dBytes, "%s", sNewText);
+  snprintf(this->sText, BUFFER_MAX_LENGTH, "%s", sNewText);
 }
 
 /**
@@ -122,17 +138,27 @@ void Buffer_setText(Buffer *this, char *sNewText) {
  * ////////// 
 */
 
+/**
+ * Clears the current contents of a buffer instance.
+ * Resets the current renderable string width to 0.
+ * 
+ * @param   { Buffer * }  this  The buffer instance to be modified.
+*/
 void Buffer_clearText(Buffer *this) {
   free(this->sText);
+
+  this->dWidth = 0;
+  this->sText = calloc(BUFFER_MAX_LENGTH, sizeof(char));
 }
 
+/**
+ * Appends text to the end of the string stored by the buffer.
+ * 
+ * @param   { Buffer * }  this      The buffer instance to be modified.
+ * @param   { char * }    sAddText  The string to be appended.
+*/
 void Buffer_addText(Buffer *this, char *sAddText) {
-  int dBytes = snprintf(NULL, 0, "%s%s", this->sText, sAddText) + 1;
-
-  char *sNewText = malloc(dBytes);
-
-  Buffer_clearText(this);
-  this->sText = sNewText;
+  snprintf(this->sText, BUFFER_MAX_LENGTH, "%s%s", this->sText, sAddText);
 }
 
 #endif
